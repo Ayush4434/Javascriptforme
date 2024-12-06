1139902a5baa7ae0937f32f45835022c3feddac9
Ayush4434 <146090632+Ayush4434@users.noreply.github.com>
2024-12-06 05:38:28 +0000

    variables in js

---
+++ jsallfiles/variables.js
@@ -0,0 +1,14 @@
+const accountId = "12345"
+let accountName = "Ayush"
+var accountEmail = 'ayush@techdefence.com'
+accountCity = 'Vadodara'
+
+//let local scope var global scope do not use var
+
+//accountId = '23r44' const value can't change
+// primitive data types in js :- number,string,null,undefined,Boolean,Symbol,Bigint,non primitive:- object
+accountName = 'Chauhan'
+accountEmail =  "hi"
+accountCity = 'MBD'
+
+console.table([accountId,accountName,accountEmail,accountCity])
