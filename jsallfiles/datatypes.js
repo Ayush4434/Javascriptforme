45a430472bdad0d7224a8e80cf1b7b98b65d2012
Ayush4434 <146090632+Ayush4434@users.noreply.github.com>
2024-12-10 05:08:31 +0000

    datatypes

--- jsallfiles/datatypes.js
+++ jsallfiles/datatypes.js
@@ -4,5 +4,14 @@
 
 console.log(3+
     3
-)
+)//code readabilty should be high
 console.log("Hello World!")
+
+// number=> 2 to pwr 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//symbol => unique
+//object => 
+// typeof null is object and typeof undefined is undefined
