f34fce7b564e1a30ef3da9f5ae4fd1291161ebe9
Ayush4434 <146090632+Ayush4434@users.noreply.github.com>
2024-12-10 22:30:30 +0000

    numbers and methods

---
+++ jsallfiles/numsandmath.js
@@ -0,0 +1,14 @@
+let num = 1334543534
+console.log(num)
+const n = new Number(12332432)
+console.log(n)
+console.log(n.toString().length)
+console.log(n.valueOf())
+let onum = 128.95344
+console.log(onum.toFixed(2))
+console.log(onum.toPrecision(3))
+
+const hunderds = 1000000000000
+console.log(hunderds.toLocaleString('en-IN'))
+//practice all other methods of number in js
+//practice all maths especially randomfloor,ceil,round
